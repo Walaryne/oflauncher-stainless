81430b31baeabea49f724901f1895b41dad3b5d8
Walaryne <hazardoustheradioactive@gmail.com>
2020-06-14 04:28:14 -0400

    Added OFSNet test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "gui/OFSGui.h"
+#include "net/OFSNet.h"
 
 int main() {
     OFSGui g;
     g.init();
+    OFSNet::test();
 }
--- src/net/OFSNet.h
+++ src/net/OFSNet.h
@@ -4,10 +4,16 @@
 
 #ifndef OFLAUNCHER_STAINLESS_OFSNET_H
 #define OFLAUNCHER_STAINLESS_OFSNET_H
+#include "curl_easy.h"
+#include "curl_exception.h"
 
+using curl::curl_easy;
+using curl::curl_easy_exception;
+using curl::curlcpp_traceback;
 
 class OFSNet {
-
+public:
+	static void test();
 };
 
 
