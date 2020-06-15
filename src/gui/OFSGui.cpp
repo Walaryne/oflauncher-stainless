075319d1da6535392c0cc4e4406e1ece474829b5
Mangmasta <mangmasta@aim.com>
2020-06-15 00:55:58 -0700

    Implemented an image loading system and renderer

--- src/gui/OFSGui.cpp
+++ src/gui/OFSGui.cpp
@@ -4,6 +4,29 @@
 
 #include "OFSGui.h"
 
+OFSGuiImage::OFSGuiImage(std::string image_file, SDL_Renderer *renderer) {
+	ok = true;
+	SDL_Surface *textureSurface = SDL_LoadBMP(image_file.c_str());
+	if(textureSurface == nullptr) {
+		ok = false;
+	} else {
+		texture = SDL_CreateTextureFromSurface(renderer, textureSurface);
+		if(texture == nullptr) {
+			ok = false;
+		}
+		SDL_FreeSurface(textureSurface);
+	}
+}
+
+OFSGuiImage::~OFSGuiImage() { SDL_DestroyTexture(texture); }
+
+bool OFSGuiImage::isOk() { return ok; }
+
+void OFSGuiImage::renderCopy(SDL_Renderer *renderer) {
+	// TODO: Later edit the null params to scale and position the texture.
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+}
+
 OFSGui::OFSGui() {
 	e_quit = false;
 	ok = true;
@@ -11,24 +34,43 @@ OFSGui::OFSGui() {
 	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
 		setError("Error initializing SDL: ");
 	} else {
-		window = SDL_CreateWindow("Open Fortress Launcher", SDL_WINDOWPOS_UNDEFINED,
-							 SDL_WINDOWPOS_UNDEFINED, 640, 480,
-							 SDL_WINDOW_SHOWN);
+		window = SDL_CreateWindow(
+			"Open Fortress Launcher", SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED, 640, 360, SDL_WINDOW_SHOWN);
 		if(!window) {
 			setError("Error initializing SDL window: ");
 		} else {
-			surface = SDL_GetWindowSurface(window);
+			surface = SDL_GetWindowSurface(window); // depricated
+			renderer = SDL_CreateRenderer(window, -1,
+										  SDL_RENDERER_ACCELERATED |
+											  SDL_RENDERER_PRESENTVSYNC);
+			if(renderer == nullptr) {
+				setError("Could not initialize Renderer: ");
+			} else
+				SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0xFF, 0xFF);
 		}
 	}
+	if(isOk()) {
+		// TODO: eventually have a way to automatically load in all needed
+		// images
+		imgs.push_back(
+			std::make_unique<OFSGuiImage>("../res/bg.bmp", renderer));
+	}
+	for(auto &x : imgs) {
+		ok &= x->isOk();
+	}
+	if(!isOk())
+		setError("Error loading resources: ");
 }
 
 OFSGui::~OFSGui() {
-	// TODO: Clean up all the surfaces and textures loaded here
+	imgs.clear();
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
 
-void OFSGui::setError(const std::string& err_msg_pre) {
+void OFSGui::setError(const std::string &err_msg_pre) {
 	ok = false;
 	err = err_msg_pre + std::string(SDL_GetError());
 }
@@ -40,9 +82,13 @@ std::string OFSGui::getError() { return err; }
 bool OFSGui::loop() {
 	SDL_Event e;
 
-	SDL_FillRect(surface, nullptr, SDL_MapRGB(surface->format, 0xFF, 0x00, 0xFF));
+	SDL_UpdateWindowSurface(window); // depricated.  delete later if need to
 
-	SDL_UpdateWindowSurface(window);
+	SDL_RenderClear(renderer);
+	for(auto &i : imgs) {
+		i->renderCopy(renderer);
+	}
+	SDL_RenderPresent(renderer);
 
 	while(SDL_PollEvent(&e)) {
 		switch(e.type) {
--- src/gui/OFSGui.h
+++ src/gui/OFSGui.h
@@ -6,12 +6,30 @@
 #define OFLAUNCHER_STAINLESS_OFSGUI_H
 
 #include "SDL2/SDL.h"
+#include <memory>
 #include <string>
+#include <vector>
+
+class OFSGuiImage {
+private:
+	SDL_Texture *texture;
+	bool ok;
+
+public:
+	OFSGuiImage(std::string image_file, SDL_Renderer *renderer);
+	~OFSGuiImage();
+	bool isOk();
+	SDL_Texture *getTexture();
+	void renderCopy(SDL_Renderer *renderer);
+};
 
 class OFSGui {
 private:
 	SDL_Window *window;
-	SDL_Surface *surface;
+	SDL_Surface *surface; // depricated because of renderer.  maybe delete later
+						  // if its seriously not needed
+	SDL_Renderer *renderer;
+	std::vector<std::unique_ptr<OFSGuiImage>> imgs;
 	bool ok;
 	std::string err{};
 	bool e_quit;
