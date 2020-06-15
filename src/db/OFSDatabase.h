2a141515c0840963c5a8e620ab0794366fc8229a
Walaryne <hazardoustheradioactive@gmail.com>
2020-06-15 03:57:17 -0400

    Prepare for Database integration

---
+++ src/db/OFSDatabase.h
@@ -0,0 +1,10 @@
+//
+// Created by walaryne on 6/15/20.
+//
+
+#ifndef OFLAUNCHER_STAINLESS_OFSDATABASE_H
+#define OFLAUNCHER_STAINLESS_OFSDATABASE_H
+
+class OFSDatabase {};
+
+#endif // OFLAUNCHER_STAINLESS_OFSDATABASE_H
