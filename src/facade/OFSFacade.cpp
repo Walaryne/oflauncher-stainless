27b296e988c626af4f1e63780d05a45f3baea41a
Walaryne <hazardoustheradioactive@gmail.com>
2020-06-29 04:11:27 -0400

    Removed the ofsdll stuff

--- src/facade/OFSFacade.cpp
+++ src/facade/OFSFacade.cpp
@@ -3,8 +3,18 @@
 //
 
 #include "OFSFacade.h"
+#include "../gui/OFSGui.h"
+
+void testFunc() {
+	std::cout << "The button is pressed" << std::endl;
+}
 
 void *CreateInterface(char const *name, int *err) {
+	OFSGui g;
+
+	g.bindActivity(BUT_CLICKED_UPDATE, testFunc);
+	while(g.loop()) {
+	}
 	std::cout << "LOL IT WORKS FAM" << std::endl;
 	return nullptr;
 }
