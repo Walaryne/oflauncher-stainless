9a385e50ea59dbcd8e3232161775a4754573ac0f
Terradice <terradice@protonmail.com>
2020-06-30 05:30:43 +0300

    added path discovery shit

---
+++ src/steam/OFSPathDiscover.cpp
@@ -0,0 +1,44 @@
+/*
+ * Created by Terradice on 6/29/20 14:20:00
+ */
+
+#include "OFSPathDiscover.h"
+
+
+#include <cstdlib>
+#include <string>
+
+#if __linux__
+const std::vector<fs::path> paths {
+    exp_env("${HOME}/.steam/steam/")
+};
+#else
+const std::vector<fs::path> paths {
+    "C:\Program Files (x86)\Steam"
+};
+#endif
+
+
+
+OFSPathDiscover::OFSPathDiscover() {
+
+    for ( auto &p : paths ) {
+        std::cout << p << std::endl;
+        if ( fs::exists(p) ) {
+            std::cout << "Found steam library: " << p << std::endl;
+
+            steamPath = p;
+
+
+            if ( fs::exists( p / "steamapps/sourcemods" ) ) {
+                std::cout << "Path also contains sourcemods dir" << std::endl;
+                sourcemodsPath = p / "steamapps/sourcemods";
+            }
+            
+            if ( fs::exists( p / "steamapps/common/Source SDK Base 2013 Multiplayer" ) ) {
+                std::cout << "Path also contains Source SDK dir" << std::endl;
+                sourcePath = p / "steamapps/common/Source SDK Base 2013 Multiplayer";
+            }
+        }
+    }
+}
---
+++ src/steam/OFSPathDiscover.h
@@ -0,0 +1,49 @@
+/*
+ * Created by Terradice on 6/29/20 14:20:00
+ */
+
+#ifndef OFLAUNCHER_STAINLESS_OFSPATH_H
+#define OFLAUNCHER_STAINLESS_OFSPATH_H
+
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <cstdlib>
+#include <string>
+
+static std::string exp_env( const std::string &s ) {
+    if( s.find( "${" ) == std::string::npos ) return s;
+
+    std::string pre  = s.substr( 0, s.find( "${" ) );
+    std::string post = s.substr( s.find( "${" ) + 2 );
+
+    if( post.find( '}' ) == std::string::npos ) return s;
+
+    std::string variable = post.substr( 0, post.find( '}' ) );
+    std::string value    = "";
+
+    post = post.substr( post.find( '}' ) + 1 );
+
+    const char *v = getenv( variable.c_str() );
+    if( v != NULL ) value = std::string( v );
+
+    return exp_env( pre + value + post );
+};
+
+namespace fs = std::filesystem;
+
+class OFSPathDiscover {
+public:
+        explicit OFSPathDiscover();
+        std::string getSteamPath();
+        std::string getSourcePath();
+        std::string getSourcemodsPath();
+private:
+        fs::path steamPath;
+        fs::path  sourcePath;
+        fs::path sourcemodsPath;
+};
+
+#endif // OFLAUNCHER_STAINLESS_OFSPATH_H
