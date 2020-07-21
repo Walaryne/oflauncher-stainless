9f96498573c3b254311daf7e1e1d23ce75401c55
Mangmasta <mangmasta@gmail.com>
2020-07-21 02:22:03 -0700

    Moved facade stuff into facade branch.

--- src/facade/dll_win/OFSLoadLib.h
+++
@@ -1,6 +0,0 @@
-#include <Windows.h>
-#define CREATEINTERFACE_PROCNAME "CreateInterface"
-
-#define DLL_EXPORT extern "C" __declspec(dllexport)
-
-void *realClientInterface(char const *name, int *err)
