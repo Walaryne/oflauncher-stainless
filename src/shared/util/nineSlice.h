1004c47d65be92143196212db51066eb16cfc6c2
Digit <woof@puppy.surf>
2020-08-04 01:17:59 -0700

    add a really basic nine-slice implementation to better tile buttons/panels/etc.
    also includes a sample image that works with the nine-slice logic
    use like this:
    
    ```
    SDL_Surface* textureSurface = SDL_CreateRGBSurfaceWithFormat(0, width, height, 32, SDL_PIXELFORMAT_RGBA32);
    blitNineSliceToSurface(renderer, spriteSurface, textureSurface, width, height);
    _texture = SDL_CreateTextureFromSurface(renderer, textureSurface);
    ```
    
    where spriteSurface is the nine-slice sprite sheet, and width and height is the desired size of the
    rendered nine-slice surface

---
+++ src/shared/util/nineSlice.h
@@ -0,0 +1,7 @@
+//
+// Created by Digit on 8/4/2020.
+//
+
+#include <SDL2/SDL.h>
+
+extern void blitNineSliceToSurface(SDL_Renderer *renderer, SDL_Surface *src, SDL_Surface *surface, int width, int height);
