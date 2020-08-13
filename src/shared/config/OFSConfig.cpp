b20490bfbd75b6b8bf1ca9bd9909f53496684776
Digit <woof@puppy.surf>
2020-08-13 00:27:57 -0700

    initial impl of a light abstraction over JSON.hpp for config files

---
+++ src/shared/config/OFSConfig.cpp
@@ -0,0 +1,123 @@
+//
+// Created by Digit on 8/10/2020.
+//
+
+#include "OFSConfig.h"
+
+OFSConfig::OFSConfig() {
+	configFilePath = getConfigFilePath();
+	std::cout << "Config path dir: " << configFilePath << std::endl;
+}
+
+/**
+ * Loads the config file from disk
+ * @return - succeeded
+ */
+bool OFSConfig::loadFromDisk() {
+	if(!std::filesystem::exists(configFilePath)) {
+		std::ofstream newFile(configFilePath);
+		newFile << DEFAULT_CONFIG_TEXT;
+		newFile.close();
+		std::cout
+			<< "Could not find existing config file, created a default one."
+			<< std::endl;
+	}
+
+	std::ifstream hdl(configFilePath);
+	if(!hdl.is_open()) {
+		return false;
+	}
+
+	std::stringstream buffer;
+	buffer << hdl.rdbuf();
+
+	try {
+		buffer >> parsedJsonDocument;
+	} catch(std::exception &e) {
+		hdl.close();
+		std::cerr << e.what() << std::endl;
+		throw std::exception(
+			"Failed to parse JSON config file, it may be corrupt.");
+	}
+
+	hdl.close();
+
+	documentLoaded = true;
+
+	return true;
+}
+
+/**
+ * Saves the config file to disk
+ * @return - succeeded
+ */
+bool OFSConfig::commitToDisk() {
+	assertDocumentLoaded();
+
+	std::ofstream hdl(configFilePath, std::ios::trunc);
+	if(!hdl.is_open()) {
+		return false;
+	}
+
+	hdl << parsedJsonDocument.dump();
+	hdl.close();
+
+	return false;
+}
+
+/**
+ * Tests if a path exits in the current loaded JSON config object
+ * @param path - path you want to test
+ * @return if path exists in loaded config
+ */
+bool OFSConfig::exists(const char *path) {
+	assertDocumentLoaded();
+	auto ptr = nlohmann::json::json_pointer(path);
+	return parsedJsonDocument.contains(ptr);
+}
+
+/**
+ * Gets the destination path for the config file
+ * @return - path of config file
+ */
+std::filesystem::path OFSConfig::getConfigFilePath() {
+	char buffer[MAX_PATH];
+	size_t len;
+
+	errno_t error = getenv_s(&len, buffer, sizeof(buffer) - 1, ENV_DIR);
+	if(error != 0) {
+		throw std::exception("Failed to get env variable for config file path");
+	}
+
+	return buffer / std::filesystem::path(CONFIG_FILE_NAME);
+}
+
+/**
+ * Will throw if a file is not currently loaded
+ */
+void OFSConfig::assertDocumentLoaded() const {
+	if(!documentLoaded) {
+		throw std::exception(
+			"Config file operator called before the file was opened.");
+	}
+}
+
+/**
+ *
+ * @param ptr - JSON_Pointer of the requested JSON path
+ * used to fill non-existent keys with null so we don't need to
+ * worry about if the key exists before insert into it.
+ *
+ * It's basically `mkdir -p` for the JSON object
+ */
+void OFSConfig::ensureJsonPtrExists(const nlohmann::json::json_pointer &ptr) {
+	std::stringstream pathString(ptr);
+	nlohmann::json *curr = &parsedJsonDocument;
+	std::string pathSegment;
+	while(std::getline(pathString, pathSegment, '/')) {
+		if(pathSegment.empty()) {
+			continue; // Skip empty path segments
+		}
+		curr = &(*curr)[pathSegment];
+	}
+}
---
+++ src/shared/config/OFSConfig.h
@@ -0,0 +1,104 @@
+//
+// Created by Digit on 8/10/2020.
+//
+
+#ifndef OFLAUNCHER_STAINLESS_OFSCONFIG_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
+#include "../util/json.hpp"
+
+#define DEFAULT_CONFIG_TEXT "{}"
+#define CONFIG_FILE_NAME ".oflauncher.config"
+
+#if __linux__
+#define ENV_DIR "HOME"
+#endif
+
+#if WIN32
+#include <windows.h>
+#define ENV_DIR "appdata"
+#endif
+
+#define OFLAUNCHER_STAINLESS_OFSCONFIG_H
+
+/**
+ * Example Usage:
+ *      OFSConfig* config = new OFSConfig();
+ *
+ *      config->loadFromDisk();
+ *
+ *      config->writeValue<const char*>("/test/value/nested/string", "hello_world");
+ *      config->writeValue<int>("/test/value/nested/int", 524);
+ *      config->writeValue<float>("/test/value/nested/float", 5.64782f);
+ *      config->writeValue<bool>("/test/value/nested/bool", true);
+ *
+ *      config->commitToDisk();
+ *
+ *      std::string outString;
+ *      config->readValue<std::string>("/test/value/nested/string", outString);
+ *
+ *      config->exists("/i/dont/exist");
+ */
+
+class OFSConfig {
+public:
+	OFSConfig();
+	~OFSConfig();
+
+	bool loadFromDisk();
+	bool commitToDisk();
+
+	bool exists(const char *path);
+
+    /**
+     * Stores a value at a specified path in the JSON config object
+     *
+     * @tparam ValueType - Type of the value you want to write (int, float, double, string, bool)
+     * @param path - Path in the config object you want to store the value at
+     * @param valueToWrite - The value you want to store
+     */
+    template <typename ValueType>
+    inline void writeValue(const char *path, const ValueType &valueToWrite) {
+        assertDocumentLoaded();
+
+        auto ptr = nlohmann::json::json_pointer(path);
+        ensureJsonPtrExists(ptr);
+
+        parsedJsonDocument.at(ptr) = valueToWrite;
+	}
+
+    /**
+     * Reads a value at a specified path in the JSON config object
+     *
+     * @tparam ValueType - Type of the value you want to write (int, float, double, string, bool)
+     * @param path - Path in the config object you want to store the value at
+     * @param valueToWrite - The value you want to store
+     */
+    template <typename ValueType>
+    inline void readValue(const char *path, ValueType &out) {
+        assertDocumentLoaded();
+
+        auto ptr = nlohmann::json::json_pointer(path);
+        if (!parsedJsonDocument.contains(ptr)) {
+            return;
+		}
+
+		out = parsedJsonDocument.at(ptr).get<ValueType>();
+    }
+
+private:
+    bool documentLoaded = false;
+    nlohmann::json parsedJsonDocument;
+	std::filesystem::path configFilePath;
+
+	void assertDocumentLoaded() const;
+	void ensureJsonPtrExists(const nlohmann::json::json_pointer& ptr);
+	static std::filesystem::path getConfigFilePath();
+};
+
+#endif // OFLAUNCHER_STAINLESS_OFSCONFIG_H
