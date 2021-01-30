20e5729f3a0539f4efc4f209eea9e0f03728b9e4
int-72h <int-10h@protonmail.com>
2021-01-30 00:04:13 +0000

    actually add the OFFilesystem header

---
+++ src/shared/util/OFFilesystem.h
@@ -0,0 +1,11 @@
+//
+// Created by int on 29/01/21.
+//
+
+#if __cplusplus >= 201703L
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
