79da1cbc6c2d2edf553f89d91c643e4357b7944e
Mangmasta <mangmasta@gmail.com>
2021-04-17 02:48:16 -0700

    Initial work to add crypto++

--- src/launcher/MD5/OFSVerifyIntegrity.cpp
+++ src/launcher/MD5/OFSVerifyIntegrity.cpp
@@ -4,19 +4,43 @@
 
 #include "OFSVerifyIntegrity.h"
 
-static hashwrapper *md5 = new md5wrapper();
+
+//static hashwrapper *md5 = new md5wrapper();
+
 
 bool verifyIntegrity(fs::path fToCheck, std::string md5db) {
+	//CryptoPP::Weak1::MD5 hash;
+
 	std::string md5ret = "";
+	std::string digest;
+	//std::ifstream fileToHash(fToCheck.make_preferred().string(), std::ios::binary | std::ios::ate);
+	//CryptoPP::HexEncoder encoder(new CryptoPP::FileSink(std::cout));
+
 	try {
+		CryptoPP::Weak::MD5 hash;
+		CryptoPP::HashFilter f(hash, new CryptoPP::HexEncoder(new CryptoPP::StringSink(digest)));
+
+		CryptoPP::FileSource(fToCheck.make_preferred().string().c_str(), true, new CryptoPP::Redirector(f));
+
+
+		md5ret = digest;
+		/*
 		md5ret =
 			md5->getHashFromFile(fToCheck.make_preferred().string());
+		 */
+
 	}
+	catch(const CryptoPP::Exception& ex){
+		std::cerr << "Hash error " << ex.what() << std::endl;
+	}
+	/*
 	catch(hlException &e) {
 		std::cerr << "hash error " << e.error_number() << ": "
 				  << e.error_message() << std::endl;
-	}
-	if( md5ret == md5db)
+	}*/
+
+	//if( md5ret == md5db)
+	if(digest == md5db)
 		return true;
 
 	std::cout << md5ret << " " << md5db << std::endl;
--- src/launcher/MD5/OFSVerifyIntegrity.h
+++ src/launcher/MD5/OFSVerifyIntegrity.h
@@ -8,7 +8,14 @@
 #include <string>
 #include "OFFilesystem.h"
 #include <iostream>
-#include "../hashlib2plus/hashlibpp.h"
+#include <fstream>
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/hex.h"
+#include "cryptopp/files.h"
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+#include "cryptopp/md5.h"
+//#include "../hashlib2plus/hashlibpp.h"
+
 
 bool verifyIntegrity(fs::path fToCheck, std::string md5db);
 
